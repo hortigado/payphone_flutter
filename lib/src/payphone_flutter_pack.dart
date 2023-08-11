2401afc0ac12bd177ee1588899c708c950bf6a92
Carlos Moises Arevalo <42924254+hortigado@users.noreply.github.com>
2023-08-11 11:44:42 -0500

    payphone redirect url

--- lib/src/payphone_flutter_pack.dart
+++ lib/src/payphone_flutter_pack.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_auth_2/flutter_web_auth_2.dart';
 import 'package:payphone/src/api/api.dart';
+/* import 'package:webview_flutter/webview_flutter.dart'; */
+import 'dart:convert';
+import 'package:flutter/services.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter/foundation.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class PayphoneWidget extends StatefulWidget {
@@ -38,11 +45,11 @@ class PayphoneWidget extends StatefulWidget {
 }
 
 class _PayphoneWidgetState extends State<PayphoneWidget> {
-/*   late WebViewXController webviewController; */
-  late WebViewController webViewController;
+  late WebViewController webviewController;
 
   String link = '';
   bool loading = true;
+  String? error;
   @override
   void didChangeDependencies() {
     loadData();
@@ -56,104 +63,152 @@ class _PayphoneWidgetState extends State<PayphoneWidget> {
       loading = true;
     });
     // Enviamos los datos
-    var response = await generateLinkPayPhone(
-        widget.amount,
-        widget.tax,
-        widget.amountWithTax,
-        widget.clientTransactionId,
-        widget.currency,
-        widget.reference,
-        widget.token);
-    setState(() {
-      link = response;
-      loading = false;
-    });
+
+    try {
+      var response = await generateLinkPayPhone(
+          widget.amount,
+          widget.tax,
+          widget.amountWithTax,
+          widget.clientTransactionId,
+          widget.currency,
+          widget.reference,
+          widget.token);
+      if (response.toString().contains("Code")) {
+        Map<String, dynamic> data = response;
+        print("aa ${data["Message"]}");
+        setState(() {
+          loading = false;
+          error = data["Message"];
+        });
+      } else {
+        setState(() {
+          link = response;
+          loading = false;
+        });
+      }
+    } catch (e) {
+      setState(() {
+        loading = false;
+        error = e.toString();
+      });
+      print(e);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    webViewController
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setBackgroundColor(const Color(0x00000000))
-      ..setNavigationDelegate(NavigationDelegate(
-        onProgress: (int progress) {
-          // Update loading bar.
-        },
-        onPageStarted: (String url) {},
-        onPageFinished: (String url) {},
-        onWebResourceError: (WebResourceError error) {},
-        onNavigationRequest: (NavigationRequest request) {
-          if (request.url ==
-              "https://pay.payphonetodoesposible.com/PayPhone/Cancelled") {
-            widget.cancelled();
-            print('cancelado');
-          }
-          if (request.url.split('Result?')[0].toString() ==
-              "https://pay.payphonetodoesposible.com/Direct/") {
-            widget.success();
-            print('pagado');
-          }
-          if (request.url.split('/')[4].toString() == "Expired") {
-            widget.cancelled();
+    /*  print(Uri.parse(link).toString()); */
 
-            print('Expirado');
-          }
-          return NavigationDecision.navigate;
-        },
-      ))
-      ..loadRequest(Uri.parse(link));
-    return Container(
-      child: loading == true
-          ? Container(
-              width: widget.width,
-              height: widget.height,
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Container(
-                      width: 40,
-                      height: 40,
-                      child: CircularProgressIndicator(
-                        color: Colors.greenAccent,
+    if (loading == false) {
+      launchUrlString(
+        "https://hordevfree.online/payphone.html",
+        mode: LaunchMode.externalNonBrowserApplication,
+      );
+
+      /*   try {
+        FlutterWebAuth2.authenticate(
+          url: "https://hordevfree.online/payphone.html",
+          callbackUrlScheme: "https://payphone-callback.hordevfree.online",
+        ).then((value) async {
+          print("aaaaa $value");
+          Uri url = Uri.parse(value);
+          final key = url.queryParameters['key'];
+        });
+      } catch (e) {
+        print(e);
+      } */
+
+      /*     webviewController = WebViewController()
+        ..setJavaScriptMode(JavaScriptMode.unrestricted)
+        ..setBackgroundColor(Color.fromARGB(0, 252, 251, 251))
+        ..setNavigationDelegate(NavigationDelegate(
+          onProgress: (int progress) {
+            // Update loading bar.
+          },
+          onPageStarted: (String url) {},
+          onPageFinished: (String url) {},
+          /*  onUrlChange: (change) {
+            if (change.url!.startsWith('intent://')) {
+              var newUrl = change.url.toString().replaceAll("intent", "https");
+
+              webViewController.loadRequest(Uri.parse(newUrl));
+            }
+          }, */
+          onWebResourceError: (WebResourceError error) {},
+          onNavigationRequest: (NavigationRequest request) async {
+            if (request.url.startsWith('intent://')) {
+              // Manejar la URL de intención personalizada aquí
+              var newUrl = request.url.toString().replaceAll("intent", "");
+
+              launchUrl(Uri.https(newUrl));
+              print(5555);
+
+              return NavigationDecision.prevent;
+            }
+
+            if (request.url ==
+                "https://pay.payphonetodoesposible.com/PayPhone/Cancelled") {
+              widget.cancelled();
+              print('cancelado');
+            }
+            if (request.url.split('Result?')[0].toString() ==
+                "https://pay.payphonetodoesposible.com/Direct/") {
+              widget.success();
+              print('pagado');
+            }
+            if (request.url.split('/')[4].toString() == "Expired") {
+              widget.cancelled();
+
+              print('Expirado');
+            }
+            return NavigationDecision.navigate;
+          },
+        ))
+        ..loadRequest(Uri.parse("https://hordevfree.online/payphone.html")); */
+    }
+
+    return Scaffold(
+        body: loading == true
+            ? Container(
+                width: widget.width,
+                height: widget.height,
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.all(8),
+                        child: Image.asset("assets/images/payphone.png",
+                            package: "payphone", width: widget.width * .2),
                       ),
-                    )
-                  ],
+                      Container(
+                        width: 40,
+                        height: 40,
+                        child: CircularProgressIndicator(
+                          color: Colors.greenAccent,
+                        ),
+                      )
+                    ],
+                  ),
                 ),
-              ),
-            )
-          : WebViewWidget(controller: webViewController),
-      /*   WebViewX(
-        initialContent: link,
-        initialSourceType: SourceType.url,
-        onWebViewCreated: (controller) => webviewController = controller,
-        height: widget.height,
-        width: widget.width,
-        navigationDelegate: (navigation) {
-          if (navigation.content.source.characters.string ==
-              "https://pay.payphonetodoesposible.com/PayPhone/Cancelled") {
-            widget.cancelled();
-            print('cancelado');
-          }
-          if (navigation.content.source.characters.string
-                  .split('Result?')[0]
-                  .toString() ==
-              "https://pay.payphonetodoesposible.com/Direct/") {
-            widget.success();
-            print('pagado');
-          }
-          if (navigation.content.source.characters.string
-                  .split('/')[4]
-                  .toString() ==
-              "Expired") {
-            widget.cancelled();
-
-            print('Expirado');
-          }
-          return NavigationDecision.navigate;
-        },
-      ), */
-    );
+              )
+            : error != null
+                ? Center(
+                    child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.all(8),
+                        child: Image.asset("assets/images/payphone.png",
+                            package: "payphone", width: widget.width * .2),
+                      ),
+                      Text(
+                        error!,
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ))
+                : Container());
   }
 }
